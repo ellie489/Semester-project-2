88521f1296d766e43da24cbf0be72393e5d904ee
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-12-11 11:51:00 +0100

    Creates auth for login and register

---
+++ src/modules/auth.mjs
@@ -0,0 +1,61 @@
+import { API_URLS } from "./constants.mjs";
+import { fetchData } from "./api.mjs"; 
+
+async function loginUser(username, password) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    };
+
+    const { success, data } = await fetchData(API_URLS.LOGIN, options);
+
+    if (!success) {
+      throw new Error('Login failed');
+    }
+
+    const { name, credits, avatar, accessToken } = data;
+
+    localStorage.setItem('name', name);
+    localStorage.setItem('credits', credits);
+    localStorage.setItem('avatar', avatar);
+    localStorage.setItem('accessToken', accessToken);
+
+    return { success: true, userData: { name, credits, avatar, accessToken } };
+  } catch (error) {
+    console.error('Login error:', error);
+    return { success: false, error: 'An error occurred during login' };
+  }
+}
+
+export { loginUser };
+
+async function registerUser(username, email, password, avatar) {
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password, avatar }),
+      };
+  
+      const { success, data } = await fetchData(API_URLS.REGISTER, options);
+  
+      if (!success) {
+        throw new Error('Registration failed');
+      }
+  
+      window.location.href = 'login.html';
+  
+      return { success: true };
+    } catch (error) {
+      console.error('Registration error:', error);
+      return { success: false, error: 'An error occurred during registration' };
+    }
+  }
+  
+  export { registerUser };
