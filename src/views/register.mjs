d6c7bd81a1fb12ec3264143cc553cf13ba65b5d2
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-12-17 21:10:23 +0100

    Adds user feedback

--- src/views/register.mjs
+++ src/views/register.mjs
@@ -1,5 +1,4 @@
 import { registerUser } from "../modules/auth.mjs";
-
 document.getElementById('registerForm').addEventListener('submit', async function(event) {
   event.preventDefault();
 
@@ -8,13 +7,33 @@ document.getElementById('registerForm').addEventListener('submit', async functio
   const password = document.getElementById('password').value;
   const avatar = document.getElementById('avatar').value;
 
+  clearMessage();
+
   const registrationResult = await registerUser(name, email, password, avatar);
 
   if (registrationResult.success) {
-    console.log('Registration successful. Redirecting to login page...', registrationResult.data);
+    showMessage('Registration successful! You can now log in to your account.');
   } else {
-
-    console.error('Registration failed:', registrationResult.error);
-
+    displayErrorMessage(registrationResult.error, 'register-feedback');
   }
 });
+
+function displayErrorMessage(error, errorElementId) {
+  const errorElement = document.getElementById(errorElementId);
+  errorElement.textContent = error;
+  errorElement.classList.add("alert", "alert-danger");
+  errorElement.style.display = 'block';
+}
+
+function showMessage(message) {
+  displayErrorMessage(message, 'register-feedback');
+  const registerFeedback = document.getElementById('register-feedback');
+  registerFeedback.classList.remove("alert-danger");
+  registerFeedback.classList.add("alert-success");
+}
+
+function clearMessage() {
+  const registerFeedback = document.getElementById('register-feedback');
+  registerFeedback.textContent = '';
+  registerFeedback.style.display = 'none';
+}
