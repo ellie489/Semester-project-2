68155b7a7c76fc18a27ddf49db6342639dc19be9
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-12-17 21:12:55 +0100

    adds funcitons to redirect user and hide banner.

--- src/modules/utils.mjs
+++ src/modules/utils.mjs
@@ -1,17 +1,25 @@
 const isLoggedIn = localStorage.getItem("accessToken");
-  
 const signInNavItem = document.getElementById('sign-in-nav-item');
 
 if (isLoggedIn) {
-    signInNavItem.innerHTML = `
-      <a href="/profile/" class="btn btn-secondary mx-3 py-0 d-flex align-items-center" role="button">
-        Profile<img src="../../icons/Profile Icon.svg" alt="profile icon" class="mx-2" />
-        
-      </a>`;
-  } else {
-    signInNavItem.innerHTML = `
-      <a href="/login/" class="btn btn-secondary mx-3 py-0 d-flex align-items-center" role="button">
-        Sign In<img src="../../icons/Profile Icon.svg" alt="sign in icon" class="mx-2" />
-        
-      </a>`;
-  }
+  signInNavItem.innerHTML = `
+    <a href="/profile/" class="btn btn-secondary mx-3 py-0 d-flex align-items-center" role="button">
+      Profile<img src="../../icons/Profile Icon.svg" alt="profile icon" class="mx-2" />
+    </a>`;
+} else {
+  signInNavItem.innerHTML = `
+    <a href="/login/" class="btn btn-secondary mx-3 py-0 d-flex align-items-center" role="button">
+      Sign In<img src="../../icons/Profile Icon.svg" alt="sign in icon" class="mx-2" />
+    </a>`;
+
+  const signInButton = signInNavItem.querySelector('a');
+  signInButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    window.location.href = '/login/'; 
+  });
+}
+
+const banner = document.getElementById('banner');
+if (!isLoggedIn) {
+  banner.style.display = 'none';
+}
